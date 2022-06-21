a8e4596657262dc06fdb73f6b25d125b6f6acc48
Elena Berestok <=>
2022-06-21 23:37:49 +0300

    item 3

--- script/script.js
+++ script/script.js
@@ -1,74 +1,68 @@
 // ДЗ пункт 1
 
-// const userAnswer = Number(prompt ('Выберите число'));
-// console.log(userAnswer);
+const userAnswer = Number(prompt ('Выберите число'));
+console.log(userAnswer);
 
 
-// const userAnswerTwo = Number(prompt ('Выберите ещё одно число'));
-// console.log(userAnswerTwo);
+const userAnswerTwo = Number(prompt ('Выберите ещё одно число'));
+console.log(userAnswerTwo);
 
-// if (userAnswer > userAnswerTwo){
-//     alert ('Число '+ userAnswer + ' больше, чем число '+ userAnswerTwo);
-// } 
-// else if (userAnswer < userAnswerTwo){
-//     alert ('Число '+ userAnswer +  ' меньше, чем число '+ userAnswerTwo);
-// }
-// else if (userAnswer = userAnswerTwo){
-//     alert ('Числa равны');
-// }
-// else {
-//     alert ('Укажите число')
-// }
+if (userAnswer > userAnswerTwo){
+    alert ('Число '+ userAnswer + ' больше, чем число '+ userAnswerTwo);
+} 
+else if (userAnswer < userAnswerTwo){
+    alert ('Число '+ userAnswer +  ' меньше, чем число '+ userAnswerTwo);
+}
+else if (userAnswer = userAnswerTwo){
+    alert ('Числa равны');
+}
+else {
+    alert ('Укажите число')
+}
 
 
 // ДЗ пункт 2
 
-// const question = Number(prompt ('Выберите число'));
-// console.log(question);
+const question = Number(prompt ('Выберите число'));
+console.log(question);
 
-// const questionTwo = Number(prompt ('Выберите ещё одно число'));
-// console.log(questionTwo);
+const questionTwo = Number(prompt ('Выберите ещё одно число'));
+console.log(questionTwo);
 
-// const questionThree = prompt ('Выберите математическое действие');
-// console.log(questionTwo);
+const questionThree = prompt ('Выберите математическое действие');
+console.log(questionTwo);
 
-// switch (questionThree){
-//     case '+':
-//         alert ( question + questionTwo);
-//         break;
-//     case '-':
-//         alert ( question - questionTwo);
-//         break;
-//     case '*':
-//         alert ( question * questionTwo);
-//         break;
-//     case '/':
-//         alert ( question / questionTwo);
-//         break;
-//     default:
-//         alert ( 'Не знаю такого действия');
-// }
+switch (questionThree){
+    case '+':
+        alert ( question + questionTwo);
+        break;
+    case '-':
+        alert ( question - questionTwo);
+        break;
+    case '*':
+        alert ( question * questionTwo);
+        break;
+    case '/':
+        alert ( question / questionTwo);
+        break;
+    default:
+        alert ( 'Не знаю такого действия');
+}
 
 // ДЗ пункт 3
 
-let userAge = Number(prompt ('Укажите свой возраст'));
+const userAge = Number(prompt ('Укажите свой возраст'));
 console.log(userAge);
 
-let minAge = 12;
+const withParents = confirm('Вы с родителями?');
 
-const withParents = prompt ( 'Вы с родителями?')
-
-if (userAge = 18-60){
+if (userAge >= 18 && userAge <= 60) {
     alert ('Доступ разрешен');
-} else if (userAge = 12-17){
-    withParents = true;
+    console.log( 'Доступ разрешен');
+} else if ((userAge >= 12 && userAge <= 80) && (withParents)) {
     alert ('Доступ разрешен');
-} else if (userAge = 12-17){
-    withParents = false;
-    alert ('Доступ запрещен');
-} else if (userAge >61){
-    alert ('Доступ запрещен');
-} 
-else {
-    alert ('Доступ разрешен')
+    console.log( 'Доступ разрешен');
+} else {
+    alert ('Доступ запрещён');
+    console.log( 'Доступ запрещён');
 }
